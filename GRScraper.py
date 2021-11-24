d6f6bf1336246b45bf794ab19041b0596cafd88f
Ian Stewart <ianstewart4outlook.com>
2021-11-23 21:24:15 -0600

    adding some code

--- GRScraper.py
+++ GRScraper.py
@@ -1,7 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "/Users/ianstewart/Desktop/Python/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://play.google.com/store/apps/details?id=com.episodeinteractive.android.catalog&hl=en&gl=US")
+
+
+search = driver.find_elements_by_class_name("")
+
+time.sleep(5)
+
 driver.quit()
